f761129c532e46fdee01bcf5a6f99d5aec94fd72
a2816 <molchanov.vsr@gmail.com>
2025-09-10 09:15:29 +0300

    Feat: Done for student

--- src/dialogs/edit_mode.py
+++ src/dialogs/edit_mode.py
@@ -1,19 +1,23 @@
 from typing import Any, TYPE_CHECKING
 
-from aiogram.types import CallbackQuery
+import re
+
+from aiogram.types import CallbackQuery, Message
 from aiogram.enums import ContentType
 
-from aiogram_dialog import Dialog, Window, DialogManager
-from aiogram_dialog.widgets.kbd import Button, Row, Next, Back
+from aiogram_dialog import Dialog, Window, DialogManager, ShowMode, StartMode
+from aiogram_dialog.widgets.kbd import Button, Row, Start, Back
 from aiogram_dialog.widgets.text import Format, Const
-from aiogram_dialog.widgets.input import TextInput, MessageInput
+from aiogram_dialog.widgets.input import TextInput, MessageInput, ManagedTextInput
 
 from ..utils.boarding_handlers import correct_name_handler, error_name_handler
 from ..utils.utils import get_middleware_data
 
 from ..google_queries import update_cell_by_coordinates
 
-from ..states import EditMode
+from ..states import EditMode, Flow
+
+from ..enums import ButtonsId
 
 from ..custom_types import Student
 
@@ -52,22 +56,63 @@ async def dialog_get_data(
     return data
 
 
-async def start_edit_photo(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_PHOTO,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
+async def start_edit_photo(
+    callback: CallbackQuery, 
+    button: Button, 
+    dialog_manager: DialogManager):
 
-async def start_edit_name(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
+    pass
+    # await dialog_manager.start(
+    #     state=EditMode.EDIT_PHOTO,
+    #     data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
+    # )
 
-    await dialog_manager.switch_to(EditMode.EDIT_NAME)
+async def start_edit_mode(
+    callback: CallbackQuery, 
+    button: Button, 
+    dialog_manager: DialogManager):
+
+    match callback.data:
+
+        case ButtonsId.EDIT_SLOGAN_BTN_ID:
+            mode = EditMode.EDIT_SLOGAN
+
+        case ButtonsId.EDIT_PROF_EXPERIENCE_BTN_ID:
+            mode = EditMode.EDIT_PROF_EXPERIENCE
+
+        case ButtonsId.EDIT_ABOUT_BTN_ID:
+            mode = EditMode.EDIT_ABOUT
+
+        case ButtonsId.EDIT_TAGS_BTN_ID:
+            mode = EditMode.EDIT_TAGS
+
+        case ButtonsId.EDIT_EXPECTATIONS_BTN_ID:
+            mode = EditMode.EDIT_EXPECTATIONS
+
+        case ButtonsId.EDIT_NAME_BTN_ID:
+            mode = EditMode.EDIT_NAME
+
+        case ButtonsId.EDIT_PHOTO_BTN_ID:
+            mode = EditMode.EDIT_PHOTO
+
+        case _:
+            pass
+
+    await dialog_manager.switch_to(mode)
 
 
-async def getter_edit_name(dialog_manager: DialogManager, **kwargs):
+async def getter_for_edition(dialog_manager: DialogManager, **kwargs):
 
     student: Student = Student(**dialog_manager.dialog_data.get("student"))
-    data: dict[str, str] = {"name": f"<b>{student.name}</b>"}
-    print(student.model_dump_json(indent=4))
+
+    data: dict[str, str] = {
+        "name": f"<b>{student.name}</b>",
+        "slogan": f"<b>{student.slogan}</b>",
+        "prof_experience": f"<b>{student.prof_experience}</b>",
+        "about": f"<b>{student.about}</b>",
+        "tags": f"<b>{student.tags}</b>",
+        "expectations": f"<b>{student.expectations}</b>"
+    }
 
     if dialog_manager.dialog_data.get("edit_mode"):
         data.update({"edit_mode": True})
@@ -75,51 +120,40 @@ async def getter_edit_name(dialog_manager: DialogManager, **kwargs):
     return data
 
 
-async def start_edit_slogan(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_SLOGAN,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
-
-async def start_edit_prof_experience(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_PROF_EXPERIENCE,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
-
-async def start_edit_about(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_ABOUT,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
-
-async def start_edit_tags(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_TAGS,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
-
-async def start_edit_expectations(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(
-        state=EditMode.EDIT_EXPECTATIONS,
-        data={"current_student_data": dialog_manager.dialog_data.get("current_student_data", 0)}
-    )
-
-
 async def process_done(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
 
-    _,config, user_data = get_middleware_data(dialog_manager)
+    _,config, _ = get_middleware_data(dialog_manager)
 
     current_state = dialog_manager.current_context().state
-    print("current_state", current_state)
+
+    row = dialog_manager.dialog_data.get("student").get("row")
 
     match current_state:
-        case EditMode.EDIT_NAME:
 
+        case EditMode.EDIT_NAME:
             column = 1
-            row = dialog_manager.dialog_data.get("student").get("row")
-            value = dialog_manager.dialog_data.get("student").get("name")
-            print("coords", column, row)
+            value = "name"
+
+        case EditMode.EDIT_SLOGAN:
+
+            column = 7
+            value = "slogan"
+
+        case EditMode.EDIT_PROF_EXPERIENCE:
+            column = 8
+            value = "prof_experience"
+
+        case EditMode.EDIT_ABOUT:
+            column = 9
+            value = "about"
+
+        case EditMode.EDIT_TAGS:
+            column = 11
+            value = "tags"
+
+        case EditMode.EDIT_EXPECTATIONS:
+            column = 14
+            value = "expectations"
 
         case _:
             pass
@@ -130,24 +164,98 @@ async def process_done(callback: CallbackQuery, button: Button, dialog_manager: 
         sheet_name=config.google.vitrina_tab,
         column=column + 1,
         row=row,
-        value=value
+        value=dialog_manager.dialog_data.get("student").get(value)
     )
 
-
     await dialog_manager.done(result={"student": dialog_manager.dialog_data.get("student")})
+
+
+async def correct_input_handler(
+        message: Message, 
+        widget: ManagedTextInput, 
+        dialog_manager: DialogManager, 
+        text: str) -> None:
+
+    current_state = dialog_manager.current_context().state
+
+    match current_state:
+
+        case EditMode.EDIT_SLOGAN:
+            value = "slogan"
+
+        case EditMode.EDIT_PROF_EXPERIENCE:
+            value = "prof_experience"
+
+        case EditMode.EDIT_ABOUT:
+            value = "about"
+
+        case EditMode.EDIT_TAGS:
+            value = "tags"
+            tokens = re.split(r"[,\s]+", text.strip())
+            text = "\n".join([t.lstrip('#') for t in tokens if t])
+
+        case EditMode.EDIT_EXPECTATIONS:
+            value = "expectations"
+
+        case _:
+            pass
+
+    dialog_manager.dialog_data["student"][value] = text
+    dialog_manager.dialog_data["edit_mode"] = True
+
+
+BACK_DONE_BTNS = Row(
+    Back(Format("{back_btn}")),
+    Button(Format("{done_btn}"), id="done_btn_id", on_click=process_done, when="edit_mode")
+    )
 
 
 dialog = Dialog(
     Window(
         Format("{edit_header}"),
-        Button(Format("{edit_photo_btn}"), id="edit_photo_btn_id", on_click=start_edit_photo),
-        Button(Format("{edit_name_btn}"), id="edit_name_btn_id", on_click=start_edit_name),
-        Button(Format("{edit_slogan_btn}"), id="edit_slogan_btn_id", on_click=start_edit_slogan),
-        Button(Format("{edit_prof_experience_btn}"), id="edit_prof_experience_btn_id", on_click=start_edit_prof_experience),
-        Button(Format("{edit_about_btn}"), id="edit_about_btn_id", on_click=start_edit_about),
-        Button(Format("{edit_tags_btn}"), id="edit_tags_btn_id", on_click=start_edit_tags),
-        Button(Format("{edit_expectations_btn}"), id="edit_expectations_btn_id", on_click=start_edit_expectations),
-        Back(Format("{back_btn}")),
+
+        Button(
+            Format("{edit_photo_btn}"), 
+            id=ButtonsId.EDIT_PHOTO_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_name_btn}"), 
+            id=ButtonsId.EDIT_NAME_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_slogan_btn}"), 
+            id=ButtonsId.EDIT_SLOGAN_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_prof_experience_btn}"), 
+            id=ButtonsId.EDIT_PROF_EXPERIENCE_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_about_btn}"), 
+            id=ButtonsId.EDIT_ABOUT_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_tags_btn}"), 
+            id=ButtonsId.EDIT_TAGS_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Button(
+            Format("{edit_expectations_btn}"), 
+            id=ButtonsId.EDIT_EXPECTATIONS_BTN_ID, 
+            on_click=start_edit_mode),
+
+        Start(
+            Format("{back_btn}"),
+            id="back_btn",
+            state=Flow.MENU,
+            show_mode=ShowMode.DELETE_AND_SEND,
+            mode=StartMode.RESET_STACK
+        ),
         state=EditMode.MAIN,
     ),
 
@@ -159,14 +267,70 @@ dialog = Dialog(
             on_success=correct_name_handler,
             on_error=error_name_handler,
         ),
-
-        Row(
-            Back(Format("{back_btn}")),
-            Button(Format("{done_btn}"), id="done_btn_id", on_click=process_done, when="edit_mode"),
-        ),
-
-        getter=getter_edit_name,
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
         state=EditMode.EDIT_NAME,
+    ),
+
+    Window(
+        Const("Текущий слоган:"),
+        Format("{slogan}"),
+        TextInput(
+            id="slogan_input",
+            on_success=correct_input_handler
+        ),
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
+        state=EditMode.EDIT_SLOGAN,
+    ),
+
+    Window(
+        Const("Текущий профессиональный опыт:"),
+        Format("{prof_experience}"),
+        TextInput(
+            id="prof_experience_input",
+            on_success=correct_input_handler
+        ),
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
+        state=EditMode.EDIT_PROF_EXPERIENCE,
+    ),
+
+    Window(
+        Const("Текущий о себе:"),
+        Format("{about}"),
+        TextInput(
+            id="about_input",
+            on_success=correct_input_handler
+        ),
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
+        state=EditMode.EDIT_ABOUT,
+    ),
+
+    Window(
+        Const("Текущие теги:"),
+        Format("{tags}"),
+        TextInput(
+            id="tags_input",
+            on_success=correct_input_handler
+        ),
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
+        state=EditMode.EDIT_TAGS,
+    ),
+
+
+    Window(
+        Const("Текущие ожидания:"),
+        Format("{expectations}"),
+        TextInput(
+            id="expectations_input",
+            on_success=correct_input_handler
+        ),
+        BACK_DONE_BTNS,
+        getter=getter_for_edition,
+        state=EditMode.EDIT_EXPECTATIONS,
     ),
 
     getter=dialog_get_data,
--- src/enums.py
+++ src/enums.py
@@ -1,6 +1,17 @@
 from enum import Enum
 
 
+class ButtonsId(str, Enum):
+    
+    EDIT_PHOTO_BTN_ID = "edit_photo_btn_id"
+    EDIT_NAME_BTN_ID = "edit_name_btn_id"
+    EDIT_SLOGAN_BTN_ID = "edit_slogan_btn_id"
+    EDIT_PROF_EXPERIENCE_BTN_ID = "edit_prof_experience_btn_id"
+    EDIT_ABOUT_BTN_ID = "edit_about_btn_id"
+    EDIT_TAGS_BTN_ID = "edit_tags_btn_id"
+    EDIT_EXPECTATIONS_BTN_ID = "edit_expectations_btn_id"
+
+
 class Action(str, Enum):
     
     START = "start"
